65e2cac3b11b8d092e081ef267c664c6a513f243
vvronskyFX <vronskymail@gmail.com>
2022-04-12 01:15:21 -0400

    first commit

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,12 +2,12 @@ export default function Footer() {
   return (
     <footer className="flex w-full justify-center border-t-2 border-gray-900 py-12 h-10">
       <a
-        href='https://www.pointer.gg?utm_source=solana-pay'
+        href='https://twitter.com/_rushin_'
         target='_blank'
         rel='noopener noreferrer'
       >
-        Learn web3 dev and earn crypto rewards at{" "}
-        <span>Pointer</span>
+        Made with 💖 by {" "}
+        <span>Vlad</span>
       </a>
     </footer>
   )
---
+++ lib/addresses.ts
@@ -0,0 +1,6 @@
+import { PublicKey } from "@solana/web3.js"
+
+// Your shop wallet address
+export const shopAddress = new PublicKey('FiRpRLrxGBsFakBtYGAzGj5LRS2vgaSzZ3Ywa7GZvgzu') 
+// USDC-Dev address
+export const usdcAddress = new PublicKey('Gh9ZwEmdLJ8DscKNTkTqPbNwLNNBjuSzaG9Vp2KGtKJr')
